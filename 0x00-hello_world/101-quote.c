37fa363f8188ca4cd46700e8fcd9a914550dad85
shamous <www.0900889670@gmail.com>
2023-09-11 15:22:22 -0700

    r

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,14 +4,13 @@
  * main - Entry point
  *
  * Description: print a qoute using write function
- * ssize_t write(int fd,const void *buf,size_t count);
+ * write(int fd,const void *buf,size_t cont)
  *
  * Return: 1 (error).
 */
 
 int main(void)
-{
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1, quo, 59);
 	return (1);
