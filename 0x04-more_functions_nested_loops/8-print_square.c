e53178b906f2625eb90c2aebb3fc767360793fa9
shamous <www.0900889670@gmail.com>
2023-09-14 16:21:25 -0700

    g

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -4,13 +4,11 @@
  * print_square - print a square, followed by new line;
  *
  * @size: is the size of the square
- *
- * Return: Always 0 (Success)
 */
 
 void print_square(int size)
 {
-	if(size <= 0)
+	if (size <= 0)
 	{
 		_putchar('\n');
 	}
@@ -22,7 +20,7 @@ void print_square(int size)
 		{
 			for (j = 0; j < size; j++)
 			{
-				_Putchar('#');
+				_putchar('#');
 			}
 			_putchar('\n');
 		}
