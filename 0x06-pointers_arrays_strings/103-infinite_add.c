e9c3ef1b8e6b93652e13624facb419c8a835db4e
shamous <www.0900889670@gmail.com>
2023-09-20 14:58:33 -0700

    h

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -47,7 +47,8 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		if (l + 2 > size_r)
 			return (0);
 		while (l-- >= 0)
-			r[0] = d + '0';
+			r[l + 1] = r[l];
+		r[0] = d + '0';
 	}
 	return (r);
 }
