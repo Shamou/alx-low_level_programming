13cf7ffebe31706c02a509555389c042f2e4cfb8
shamous <www.0900889670@gmail.com>
2023-10-11 17:04:11 -0700

    a

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,7 +1,7 @@
 #include "3-calc.h"
 
 /**
- * main - check the code for the school students.
+ * main - check the code for the School students.
  * @argc: the number of args
  * @argv: argument vector
  *
